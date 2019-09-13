87fc11ad031faf62221ad1031095cd05293a321e
Tony G. Bolaño <tonybolanyo@gmail.com>
2019-09-13 10:24:57 +0200

    Add book list to author

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,16 @@
-const graphql = require("graphql");
-const _ = require("lodash");
+const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
 // dummy data
 var books = [
-  { name: "Name of the wind", genre: "Fantasy", id: "1", authorId: '1' },
-  { name: "The final empire", genre: "Fantasy", id: "2", authorId: '2' },
-  { name: "The long earth", genre: "Sci-Fi", id: "3", authorId: '3' }
+  { name: 'Name of the wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The final empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The long earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+  { name: 'The colour of magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The light fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 
 var authors = [
@@ -36,12 +39,18 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      }
+    }
   })
 });
 
 const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
+  name: 'RootQueryType',
   fields: {
     book: {
       type: BookType,
