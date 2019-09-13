aedd5d97ce914782bd7d343ae7fe37acb8f010df
Tony G. Bolaño <tonybolanyo@gmail.com>
2019-09-13 09:32:25 +0200

    Express and basic schema setup (to lesson 11)

---
+++ README.md
@@ -0,0 +1,6 @@
+# Node + Express + React + VueJS
+
+Code for The Net Ninja GraphQL tutorial on Youtube
+
+https://www.youtube.com/playlist?list=PL4cUxeGkcC9iK6Qhn-QLcXCXPQUov1U7f
+
---
+++ server/schema/schema.js
@@ -0,0 +1,40 @@
+const graphql = require("graphql");
+const _ = require("lodash");
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+
+// dummy data
+var books = [
+  { name: "Name of the wind", genre: "Fantasy", id: "1" },
+  { name: "The final empire", genre: "Fantasy", id: "2" },
+  { name: "The long earth", genre: "Sci-Fi", id: "3" }
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: {
+        id: { type: GraphQLID }
+      },
+      resolve(parent, args) {
+        // code to get data from db / other source
+        return _.find(books, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
