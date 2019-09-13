c3ce8e6e482c7ca4c79d7029113f14b5f4c8469e
Tony G. Bolaño <tonybolanyo@gmail.com>
2019-09-13 16:54:49 +0200

    Show book details. Lesson 35

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -3,7 +3,17 @@ import { graphql } from 'react-apollo';
 
 import { getBooksQuery } from '../queries/queries';
 
+// components
+import BookDetails from "./BookDetails";
+
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
+
   displayBooks() {
     let data = this.props.data;
 
@@ -12,7 +22,7 @@ class BookList extends Component {
     } else {
       return data.books.map(book => {
         return (
-          <li key={ book.id }>{ book.name }</li>
+          <li key={ book.id } onClick={(e) => { this.setState({ selected: book.id})}}>{ book.name }</li>
         )
       })
     }
@@ -24,6 +34,7 @@ class BookList extends Component {
         <ul id="book-list">
           { this.displayBooks() }
         </ul>
+        <BookDetails bookId={ this.state.selected } />
       </div>
     );
   }
