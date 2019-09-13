3d132f3f9a412ed23feeb43a87eb5c30b1025bd2
Tony G. Bolaño <tonybolanyo@gmail.com>
2019-09-13 12:48:43 +0200

    Create BookList component placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My Reading List</h1>
-    </div>
-  );
+// Components
+import BookList from "./components/BookList";
+
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <h1>My Reading List</h1>
+        <BookList></BookList>
+      </div>
+    );
+  }
 }
 
 export default App;
