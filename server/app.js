297a42be63e5cbb226d0f55f9ac1a427fb426bd0
Tony G. Bolaño <tonybolanyo@gmail.com>
2019-09-13 11:11:38 +0200

    Add mongodb docker and connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect database
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://tony:supersegura@localhost:27017/gqlninja').then(() => {
+    'Connected to database successfully'
+  }).catch(err => {
+    console.log('Error connecting to database.')
+  });
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
